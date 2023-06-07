933b81ce6f19815dbd69bb963d32c2cd21104053
dinhdatle <111744077+dinhdatle@users.noreply.github.com>
2023-06-07 22:19:16 +0700

    reservations component

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -29,26 +29,26 @@ const ListingInfo: React.FC<ListingInfoProps> = ({user,description,guestCount,ro
     const coordinates = getByValue(locationValue)?.latlng
   return (
     <div className="col-span-4 flex flex-col gap-8">
-    <div className="flex flex-col gap-2">
-      <div className="text-xl font-semibold flex flex-row  items-center gap-2">
-        <div>Hosted by {user?.name}</div>
-        <Avatar src={user?.image} />
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex flex-row  items-center gap-2">
+          <div>Hosted by {user?.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
       </div>
-      <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
-        <div>{guestCount} guests</div>
-        <div>{roomCount} rooms</div>
-        <div>{bathroomCount} bathrooms</div>
-      </div>
+      <hr />
+      {category && (
+        <ListingCategory icon={category.icon} label={category?.label} description={category?.description} />
+      )}
+      <hr />
+      <div className="text-lg font-light text-neutral-500">{description}</div>
+      <hr />
+      <Map center={coordinates} />
     </div>
-    <hr />
-    {category && (
-      <ListingCategory icon={category.icon} label={category?.label} description={category?.description} />
-    )}
-    <hr />
-    <div className="text-lg font-light text-neutral-500">{description}</div>
-    <hr />
-    <Map center={coordinates} />
-  </div>
   )
 }
 
